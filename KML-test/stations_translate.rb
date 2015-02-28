2d3f21acd55dfb84e8888e9f6d67894dd0e4c37d
reihned <reihned@gmail.com>
2015-02-28 11:39:46 -0500

    figured out what to parse out

--- KML-test/stations_translate.rb
+++ KML-test/stations_translate.rb
@@ -7,4 +7,10 @@ stations_xml_ns = Nokogiri::XML(stations_kml)
 # parsed data into a nodeset of each station
 stations_ns = stations_xml_ns.css('Placemark')
 
+# longitude = stations_ns[0].css('longitude').text
+# latitutde = stations_ns[0].css('latitude').text
+# description = Nokogiri::HTML(stations_ns[0].css('description').text)
+# name = description.css('span')[1].text
+# lines = description.css('span')[5].text.split("-")
+
 binding.pry
